19dfee8a1c1915b4db4f9920e148f3027ef43387
github-ci <you@example.com>
2025-11-18 16:37:52 +0800

    删除无用测试用例

--- src/LockCommandBundle.php
+++ src/LockCommandBundle.php
@@ -5,12 +5,7 @@ declare(strict_types=1);
 namespace Tourze\LockCommandBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
-use Tourze\BundleDependency\BundleDependencyInterface;
 
-class LockCommandBundle extends Bundle implements BundleDependencyInterface
+class LockCommandBundle extends Bundle
 {
-    public static function getBundleDependencies(): array
-    {
-        return [];
-    }
 }
